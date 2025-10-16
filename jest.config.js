bbf6dbb0682b10464dafd2a3cc9ab5821534e53b
Petossss <petricksp121@gmail.com>
2025-10-16 13:58:00 -0300

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text-summary'],
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-};
